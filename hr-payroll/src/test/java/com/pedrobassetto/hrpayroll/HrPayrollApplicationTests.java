cc36f7b394417afd9e74ff016231d1fb0e29acf4
Pedro Bassetto <pedrobassetto1997@gmail.com>
2020-11-12 22:18:03 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/pedrobassetto/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrobassetto.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
