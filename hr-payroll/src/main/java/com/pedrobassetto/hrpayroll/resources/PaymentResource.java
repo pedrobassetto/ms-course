b46f3a6fb072d022635e89c57a953b01ffa47b2f
Pedro Bassetto <pedrobassetto1997@gmail.com>
2020-11-19 20:09:09 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/pedrobassetto/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/pedrobassetto/hrpayroll/resources/PaymentResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.pedrobassetto.hrpayroll.entities.Payment;
 import com.pedrobassetto.hrpayroll.services.PaymentService;
 
@@ -17,10 +18,17 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternativa")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
+	
+	public ResponseEntity<Payment> getPaymentAlternativa(Long workerId, Integer days){
+		Payment payment = new Payment("Pedro", 400.0, days);
+		return ResponseEntity.ok(payment);
+	}
+
 
 }
