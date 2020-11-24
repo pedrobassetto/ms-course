fca93b400d9e194de6c4c7131f69d452c1989435
Pedro Bassetto <pedrobassetto1997@gmail.com>
2020-11-23 21:45:36 -0300

    Database seeding

---
+++ hr-user/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Pedro Bassetto', 'pedro@gmail.com', '$2a$10$XwApgCLv5gu5WTDqBZ39OOYJSoc0rtvCAff3keJVaa3qfmFTdP2pi');
+INSERT INTO tb_user (name, email, password) VALUES ('Leia Red', 'leia@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+
+INSERT INTO tb_role (role_name) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (role_name) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
