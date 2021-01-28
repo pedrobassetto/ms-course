51652d9fd6897b33e2664ed532b5f6fa815ed0d1
Pedro Bassetto <pedrobassetto1997@gmail.com>
2021-01-28 20:06:39 -0300

    Login, JWT token

--- hr-user/src/main/resources/data.sql
+++ hr-user/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Pedro Bassetto', 'pedro@gmail.com', '$2a$10$XwApgCLv5gu5WTDqBZ39OOYJSoc0rtvCAff3keJVaa3qfmFTdP2pi');
+INSERT INTO tb_user (name, email, password) VALUES ('Pedro Bassetto', 'pedro@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
 INSERT INTO tb_user (name, email, password) VALUES ('Leia Red', 'leia@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
 
 INSERT INTO tb_role (role_name) VALUES ('ROLE_OPERATOR');
