79301ee996beb78a224b1fd0c41c2152ee0d9048
Pedro Bassetto <pedrobassetto1997@gmail.com>
2021-01-31 18:30:44 -0300

    Reset Project - Security configs to config server

--- hr-worker/src/main/java/com/pedrobassetto/hrworker/repositories/WorkerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pedrobassetto.hrworker.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pedrobassetto.hrworker.entities.Worker;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long>{
-
-}
